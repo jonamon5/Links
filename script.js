034f66669da21cb02bad5e6cd9ce21caf11cd16e
Jonathan Chiang <chiaj413@newschool.edu>
2025-02-05 11:37:05 -0500

    added are.na js file and linked to main.html

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 
-  
+  let checkerboard = document.getElementById("checkerboard");
+  let row = 20;
+  let column = 20;
+  let threshold = 1;
+
+  let cells =[];
+  let count = [];
+
+  console.log("this code is working")
